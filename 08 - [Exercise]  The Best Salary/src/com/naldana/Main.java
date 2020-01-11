a4e306b162c8484c7b007f1613fdeae999cbc04e
Néstor Aldana <nestor.aldana1@gmail.com>
2020-01-11 09:38:41 -0600

    Add Solution of The Best Salary

--- 08 - [Exercise]  The Best Salary/src/com/naldana/Main.java
+++ 08 - [Exercise]  The Best Salary/src/com/naldana/Main.java
@@ -5,12 +5,15 @@ public class Main {
     // TODO (1): You have lucky!, oder company offer to you a new job. Make a program to find out the best salary.
     public static void main(String[] args) {
         // TODO (2): Declare three variable a, b, and m. Initialize with the values 900, 1000 and 0 respectively.
-
-
+        double a = 900,
+                b = 1000,
+                m = 0; // The maximum salary
         // TODO (3): Create a String variable r, it should contain the output message  to be printed. you can be creative.
             // "The best option of AA and BB is MM
+        String r = "The best option of " + a + " and " + b + " is ";
         // TODO (4): Use max() method to obtain the maximum of the two salary value and store in m.
-
+        m = Math.max(a,b);
         // TODO (5): Print the results.
+        System.out.println(r + m);
      }
 }
