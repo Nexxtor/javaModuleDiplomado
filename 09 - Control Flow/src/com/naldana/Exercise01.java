ef09bd53a37eaba37c52d1bf691107a54ae4bc76
Néstor Aldana <nestor.aldana1@gmail.com>
2020-01-15 14:34:47 -0600

    Add excecise01

---
+++ 09 - Control Flow/src/com/naldana/Exercise01.java
@@ -0,0 +1,23 @@
+package com.naldana;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+        boolean hungry = false;
+        boolean happy = true;
+
+        if (happy)
+            System.out.println("I'm happy");
+
+        // Usually put the conditional code into a block
+        if (hungry) {
+            // You will not see this.
+            System.out.println("I'm hungry");
+        }
+        // TODO (1): WARNING: Unlike other languages, indentation doesn't count in Java
+
+        if ( happy ) {
+
+        }
+    }
+}
