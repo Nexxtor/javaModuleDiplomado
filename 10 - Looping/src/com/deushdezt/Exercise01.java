6a53bc490c5c9badf37ee91994d75c0ae5a10643
DouglasHdezT <00004516@uca.edu.sv>
2020-01-16 15:49:09 -0600

    Adding exercises for lopping problems... Needed: Do-while examples

---
+++ 10 - Looping/src/com/deushdezt/Exercise01.java
@@ -0,0 +1,19 @@
+package com.deushdezt;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Printing numbers from 0 to 19");
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Iteration: " + i);
+        }
+
+        System.out.println("\nPrinting numbers from 1 to 20");
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Iteration: " + (i + 1));
+        }
+    }
+}
---
+++ 10 - Looping/src/com/deushdezt/Exercise02.java
@@ -0,0 +1,17 @@
+package com.deushdezt;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        System.out.println("Printing numbers from 19 to 0");
+
+        for (int i = 19; i >= 0; i--) {
+            System.out.println("Iteration: " + i);
+        }
+
+        System.out.println("\nPrinting numbers from 20 to 1");
+
+        for (int i = 19; i >= 0; i--) {
+            System.out.println("Iteration: " + (i + 1));
+        }
+    }
+}
