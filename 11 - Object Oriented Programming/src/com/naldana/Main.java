a098a294f337aabc9cc64ecd446e20c631e66589
Néstor Aldana <nestor.aldana1@gmail.com>
2020-01-16 16:05:05 -0600

    Add two ways more

--- 11 - Object Oriented Programming/src/com/naldana/Main.java
+++ 11 - Object Oriented Programming/src/com/naldana/Main.java
@@ -3,11 +3,14 @@ package com.naldana;
 public class Main {
 
     public static void main(String[] args) {
-	    Vehicle car = new Vehicle(4,100);
+	    Vehicle car = new Vehicle();
 	    Vehicle biky =  new Vehicle();
 
 	    biky.quantityWheel =  2;
-	    biky.setMaxSpeed(20);
+		biky.maxSpeed = 20;
+
+		car.setMaxSpeed(200);
+		car.setQuantityWheel(4);
 
 	    // TODO: Print in console the car and bike data in console
     }
