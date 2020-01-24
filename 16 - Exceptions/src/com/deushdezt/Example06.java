7b436db815d65f710fe94ffcc715c91d73e77bf4
DouglasHdezT <00004516@uca.edu.sv>
2020-01-23 19:44:33 -0600

    Fix: My Exception finally block

--- 16 - Exceptions/src/com/deushdezt/Example06.java
+++ 16 - Exceptions/src/com/deushdezt/Example06.java
@@ -2,27 +2,29 @@ package com.deushdezt;
 
 import com.deushdezt.exceptions.MyException;
 
-import java.util.Arrays;
-
 public class Example06 {
     public static void main(String[] args) {
         String message = null;
+        //message = "Test message!";
         int count = -1;
 
         try {
             count = getCharCount(message);
         } catch (MyException e) {
             System.out.println(e.getMessage());
-            count = 0;
         } finally {
-            System.out.println("Count: "+ count);
+            if (count < 0){
+                count = 0;
+            }
+
+            System.out.println("Count: " + count);
         }
     }
 
     public static int getCharCount(String message) throws MyException {
-        if(message != null){
+        if (message != null) {
             return message.length();
-        }else{
+        } else {
             throw new MyException("The string is null");
         }
     }
