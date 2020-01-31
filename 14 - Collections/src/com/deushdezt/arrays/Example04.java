9d3bc0edde992d946478cb2d081d7f1754bc8753
Néstor Aldana <nestor.aldana1@gmail.com>
2020-01-31 09:15:48 -0600

    Fix: La busqueda de un palabras, se ordeno el arreglo

--- 14 - Collections/src/com/deushdezt/arrays/Example04.java
+++ 14 - Collections/src/com/deushdezt/arrays/Example04.java
@@ -6,6 +6,7 @@ public class Example04 {
     public static void main(String[] args) {
         String[] text = {"love","is", "in", "the", "air"};
 
+        Arrays.sort(text);
         int search = Arrays.binarySearch(text, "is");
         System.out.println(search);
     }
